935256e41442bd2f1cc41f21e8e2c3256cbbf979
Tong Li <tl2204@nyu.edu>
2022-04-20 00:32:20 -0400

    up

--- spark/shell_setup.sh
+++ spark/shell_setup.sh
@@ -5,6 +5,6 @@ export HADOOP_STREAMING='hadoop-mapreduce/hadoop-streaming.jar'
 alias hfs="$HADOOP_EXE fs"
 alias hjs="$HADOOP_EXE jar $HADOOP_LIBPATH/$HADOOP_STREAMING"
 
-module load python/gcc/3.8.7
+module load python/gcc/3.7.9
 
 alias spark-submit='PYSPARK_PYTHON=$(which python) spark-submit'
